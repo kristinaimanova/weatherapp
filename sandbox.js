3bfd257671c7e904de7c1a5dcc3b6e6329602265
Justin Dennison <justin1dennison@gmail.com>
2017-02-23 19:04:01 -0500

    We added axios to the dependencies and used it to make an AJAX GET request. Additionally, we learned to construct a based CommonJS module that will allow us to import functions/values into other files. We hide our api key for the darksky api in secrets.js module but ignored to keep it out of version control. Added a template for the secrets file as a reminder for others or for the future.

---
+++ sandbox.js
@@ -0,0 +1,26 @@
+var axios = require('axios');
+var apiKey = require('./secrets').darkskyAPIKey;
+var url = `https://api.darksky.net/forecast/${apiKey}/37.8267,-122.4233`;
+// var sample = require('./sampleModule');
+// console.log(sample);
+// sample.b();
+// sample.a();
+// var promiseOfData = axios.get(url);
+// promiseOfData
+//     .then(function(response){
+//       console.log(response.data)
+//     }) //success
+//     .catch(function(err){
+//       console.log(err);
+//     }); //failure or error
+
+axios.get(url)
+     .then(function(response){
+       return response.data;
+     })
+     .then(function(data){
+       console.log(data);
+     })
+     .catch(function(err){
+       console.log(err);
+     });
