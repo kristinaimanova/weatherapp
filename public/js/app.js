d9814c3ae59442ee46f8b3b13a9816ea90b96c0a
Kristina Ivanova <kristinaivanova@Kristinas-Air.itpro.tv>
2017-02-28 19:32:45 -0500

    added the axios to the application. was able to retrieve dynamic data from our server side weather route and have our vue application render that data to the screen

--- public/js/app.js
+++ public/js/app.js
@@ -15,5 +15,22 @@ var currentlyWidget = new Vue({
     precipProbability: 0.30,
     humidity: 0.61,
     location: 'Gainesville, FL'
-  }
+},
+    created: function(){
+        axios.get('/weather/29.1,-81.4')
+        .then(function(response){
+            currentlyWidget.time = response.data.currently.time;
+            currentlyWidget.summary = response.data.currently.summary;
+            currentlyWidget.icon = response.data.currently.icon;
+            currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
+            currentlyWidget.precipProbability = response.data.currently.precipProbability;
+            currentlyWidget.humidity = response.data.currently.humidity;
+            currentlyWidget.location = response.data.currently.location;
+            console.log(response.data);
+
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+    }
 });
