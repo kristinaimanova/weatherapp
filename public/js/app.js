e058a1813e320e4907d9d3878ad0b6c257edfa33
Kristina Ivanova <kristinaivanova@Kristinas-Air.itpro.tv>
2017-03-01 20:19:40 -0500

    added inputs and lets the user select the latitude and longitude

--- public/js/app.js
+++ public/js/app.js
@@ -14,15 +14,37 @@ var currentlyWidget = new Vue({
     apparentTemperature: 67.4,
     precipProbability: 0.30,
     humidity: 0.61,
-    location: 'Gainesville, FL'
+    location: 'Gainesville, FL',
+    latitude: 29.1,
+    longitude: -81.4
 },
 methods: {
         iconUrl: function(iconString){
             return `/images/${iconString}.png`;
+        },
+        updateWeather: function(latitude, longitude){
+        var url = `/weather/${currentlyWidget.latitude},${currentlyWidget.longitude}`;
+        console.log(url);
+        axios.get(url)
+        .then(function(response){
+            currentlyWidget.time = response.data.currently.time;
+            currentlyWidget.summary = response.data.currently.summary;
+            currentlyWidget.icon = response.data.currently.icon;
+            currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
+            currentlyWidget.precipProbability = response.data.currently.precipProbability;
+            currentlyWidget.humidity = response.data.currently.humidity;
+            currentlyWidget.location = response.data.currently.location;
+            console.log(response.data);
+
+        })
+        .catch(function(err){
+            console.log(err);
+        });
         }
 },
     created: function(){
-        axios.get('/weather/29.1,-81.4')
+        //
+        axios.get(currentlyWidget.url)
         .then(function(response){
             currentlyWidget.time = response.data.currently.time;
             currentlyWidget.summary = response.data.currently.summary;
