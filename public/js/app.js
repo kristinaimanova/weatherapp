ef0f9f1795d6a1f4cecd40c5be47153664b71fce
Kristina Ivanova <kristinaivanova@Kristinas-Air.itpro.tv>
2017-03-02 19:49:03 -0500

    making an hourly widget

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,41 @@ var app = new Vue({
     message: 'Hello Vue'
   }
 });
-
+var hourlyWidget = new Vue({
+    el: '#hourly',
+    data: {
+            summary: "it's gonna rain!",
+            icon: 'clear-night.png',
+            hours: []
+    },
+    methods: {
+        getHourlyIcon: function(iconString){},
+        getDate: function(){
+            var d = new Date();
+            var blahblah = d.setUTCSeconds(1488501457);
+            console.log(blahblah);
+        },
+        getMainIcon: function(){
+            return `/images/${this.icon}.png`;
+        },
+        getHourlyWeather: function(lat, lon){
+            var url = `/weather/${lat},${lon}`;
+            axios.get(url)
+            .then(function(response){
+                var hourlyData = response.data.hourly;
+                this.summary = hourlyData.summary;
+                this.icon = hourlyData.icon;
+                this.hours = hourlyData.data;
+            }.bind(this))
+            .catch(function(err){
+                console.log(err);
+            });
+        }
+    },
+    created: function(){
+        this.getHourlyWeather(29.1, -84.1);
+    }
+});
 var currentlyWidget = new Vue({
   el: '#currently',
   data: {
@@ -20,44 +54,33 @@ var currentlyWidget = new Vue({
 },
 methods: {
         iconUrl: function(iconString){
-            return `/images/${iconString}.png`;
+            return `/images/${iconString}.svg`;
+        },
+        getWeather: function(lat, lon){
+            var url = `/weather/${lat},${lon}`;
+            axios.get(url)
+            .then(function(response){
+                currentlyWidget.time = response.data.currently.time;
+                currentlyWidget.summary = response.data.currently.summary;
+                currentlyWidget.icon = response.data.currently.icon;
+                currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
+                currentlyWidget.precipProbability = response.data.currently.precipProbability;
+                currentlyWidget.humidity = response.data.currently.humidity;
+                currentlyWidget.location = response.data.currently.location;
+                console.log(response.data);
+            })
+            .catch(function(err){
+                console.log(err);
+            });
         },
         updateWeather: function(latitude, longitude){
-        var url = `/weather/${currentlyWidget.latitude},${currentlyWidget.longitude}`;
-        console.log(url);
-        axios.get(url)
-        .then(function(response){
-            currentlyWidget.time = response.data.currently.time;
-            currentlyWidget.summary = response.data.currently.summary;
-            currentlyWidget.icon = response.data.currently.icon;
-            currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
-            currentlyWidget.precipProbability = response.data.currently.precipProbability;
-            currentlyWidget.humidity = response.data.currently.humidity;
-            currentlyWidget.location = response.data.currently.location;
-            console.log(response.data);
 
-        })
-        .catch(function(err){
-            console.log(err);
-        });
+            this.getWeather(this.latitude,this.longitude);
+
         }
 },
     created: function(){
         //
-        axios.get(currentlyWidget.url)
-        .then(function(response){
-            currentlyWidget.time = response.data.currently.time;
-            currentlyWidget.summary = response.data.currently.summary;
-            currentlyWidget.icon = response.data.currently.icon;
-            currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
-            currentlyWidget.precipProbability = response.data.currently.precipProbability;
-            currentlyWidget.humidity = response.data.currently.humidity;
-            currentlyWidget.location = response.data.currently.location;
-            console.log(response.data);
-
-        })
-        .catch(function(err){
-            console.log(err);
-        });
+        this.getWeather(29.1,-81.4);
     }
 });
